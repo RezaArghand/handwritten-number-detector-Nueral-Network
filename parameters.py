ef31d1bc4a8de28f272ce13a301e50e0d0e3c523
mrrigidbody <arghand.eng@gmail.com>
2022-02-21 20:27:41 +0330

    first commitment

---
+++ parameters.py
@@ -0,0 +1,18 @@
+
+
+# parameters of PSO optimization algorithm
+number_of_particles = 120  # particle count
+
+varNum = 10  # variable count
+damping_rate_W = 0.9  # inertia damper
+max_of_variable = 10  # max domain
+min_of_variable = -10  # min domain
+satisfaction_cost_number = 1.0e-200  # satisfaction point
+W = 0.95  # inertia
+C1 = 1.8  # cognitive (particle)
+C2 = 2.4  # social (swarm)
+max_iteration_number = 3000  # max iteration
+# end parameters of PSO optimization
+
+# ll = [1, 1, 1, 1, 1, 1]
+# print(len(ll))
